eaed44f34bbc55d7685cb4b4f360aefb61bd56ba
nora kramer <nora.kramer2@hva.nl>
2022-04-21 10:31:52 +0200

    speech to text on page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-console.log('vibes')
+ const texts = document.querySelector(".texts");
 
-const textArea = document.querySelector('textarea');
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener('result', (e) => {
+ //console.log(e.results);
+
+ const text = Array.from(e.results)
+ .map(result => result[0])
+ .map(result => result.transcript)
+ .join('');
+
+ p.innerHTML = text;
+
+ texts.appendChild(p);
+
+
+ console.log(text)
+});
+
+// recognition.addEventListener("end", () => {
+//   recognition.start();
+// });
+
+recognition.start();
+
+// bron: https://www.youtube.com/watch?v=-k-PgvbktX4
